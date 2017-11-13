473ce47dd196bf20b5a94e558da1dab956a89596
zrhkc7 <zrhkc7@mail.missouri.edu>
2017-11-12 21:52:25 -0600

    Final Project

--- Color-Picker/Color-Picker/ColorPickerViewController.swift
+++ Color-Picker/Color-Picker/ColorPickerViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ColorPickerViewController: UIViewController {
+class ColorPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var colorTitle: UILabel!
+    @IBOutlet weak var colorPickerView: UIPickerView!
+    @IBOutlet var viewBackground: UIView!
+    
+    let colors = ["Red",
+                 "Orange",
+                 "Yellow",
+                 "Green",
+                 "Blue",
+                 "Purple"]
+    var selectedColor: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        colorPickerView.delegate = self
+        colorPickerView.dataSource = self
+        colorPickerView.backgroundColor = UIColor.red
+        viewBackground.backgroundColor = UIColor.red
+        colorTitle.text = "Red"
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +37,69 @@ class ColorPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedColor = colors[row]
+        colorTitle.text = selectedColor
+        if( row == 0 )
+        {
+            colorPickerView.backgroundColor = UIColor.red
+            viewBackground.backgroundColor = UIColor.red
+        }
+        if( row == 1 )
+        {
+            colorPickerView.backgroundColor = UIColor.orange
+            viewBackground.backgroundColor = UIColor.orange
+        }
+        if( row == 2 )
+        {
+            colorPickerView.backgroundColor = UIColor.yellow
+            viewBackground.backgroundColor = UIColor.yellow
+        }
+        if( row == 3 )
+        {
+            colorPickerView.backgroundColor = UIColor.green
+            viewBackground.backgroundColor = UIColor.green
+        }
+        if( row == 4 )
+        {
+            colorPickerView.backgroundColor = UIColor.blue
+            viewBackground.backgroundColor = UIColor.blue
+        }
+        if( row == 5 )
+        {
+            colorPickerView.backgroundColor = UIColor.purple
+            viewBackground.backgroundColor = UIColor.purple
+        }
+        
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
